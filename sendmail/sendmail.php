f7d8b223aa4f22cc8b2601310016479b9e58d25a
Yurii Denyshchenko <yurii.denyshchenko@classyllama.com>
2023-10-19 16:12:06 +0300

    Fix for emails

--- sendmail/sendmail.php
+++ sendmail/sendmail.php
@@ -6,6 +6,6 @@ while (false !== ($line = fgets(STDIN))) {
     $mail .= $line;
 }
 
-$mail = preg_replace(['/\=3D/', '/\=0A/', '/\=\s+/', '/\=0D/', '/\=20/', '/\=C2/','/\=AE/'], ['=', ' ', '', ' ', ' ', ' ', ' '], $mail);
+$mail = preg_replace(['/\=3D/', '/\=0A/', '/\=\s+|\=0D|\=20|\=C2|\=AE|\=E2|\=80|\=AF/'], ['=', ' ', ''], $mail);
 file_put_contents('/Users/rand/Sites/_MAILS/mail.' . time() . '.html', $mail . PHP_EOL, FILE_APPEND);
 echo 1;
