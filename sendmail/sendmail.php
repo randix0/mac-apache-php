1ea4b9d192416a5cdfdcdb5e0f09dfe1d4670071
Yurii Denyshchenko <yurii.denyshchenko@classyllama.com>
2025-12-15 16:11:33 +0200

    Updating the sendmail script

--- sendmail/sendmail.php
+++ sendmail/sendmail.php
@@ -6,6 +6,6 @@ while (false !== ($line = fgets(STDIN))) {
     $mail .= $line;
 }
 
-$mail = preg_replace(['/\=3D/', '/\=0A/', '/\=\s+|\=0D|\=20|\=C2|\=AE|\=E2|\=80|\=AF/', '/\=2E/'], ['=', ' ', '', '.'], $mail);
+$mail = preg_replace(['/\=3D/', '/\=0A/', '/\=\s+|\=0D|\=20|\=C2|\=AE|\=E2|\=80|\=AF/', '/\=99/'], ['=', ' ', '', '`'], $mail);
 file_put_contents('/Users/rand/Sites/_MAILS/mail.' . time() . '.html', $mail . PHP_EOL, FILE_APPEND);
 echo 1;
